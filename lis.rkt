edfc62f01cf757758919c0c9834931590afdebc8
Tyromancer <yangb7@rpi.edu>
2020-10-28 17:09:38 -0400

    Create lis.rkt

---
+++ lis.rkt
@@ -0,0 +1,15 @@
+;; Contract: lis_slow : list_of_numbers -> list_of_numbers
+;; Purpose: to compute the greates non-descending subsequence of the input list in O(2^n) time
+;; Example: (lis_slow `(1 2 3 4 1)) should produce (1 2 3 4)
+;; Definition
+(define (lis_slow lst)
+
+)
+
+;; Contract: lis_fast : list_of_numbers -> list_of_numbers
+;; Purpose: to compute the greates non-descending subsequence of the input list in polynomial time
+;; Example: (lis_fast `(1 2 3 4 1)) should produce (1 2 3 4)
+;; Definition
+(define (lis_fast lst)
+
+)
